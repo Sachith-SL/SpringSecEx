11cc784a7871aeba061ac08d74c87e74edaee714
Sachith-SL <itsmesachith@gmail.com>
2025-01-18 06:49:13 +0530

    add bcript implementation

---
+++ src/main/java/com/sachith/SpringSecEx/service/UserService.java
@@ -0,0 +1,21 @@
+package com.sachith.SpringSecEx.service;
+
+import com.sachith.SpringSecEx.model.User;
+import com.sachith.SpringSecEx.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public User register(User user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+}
